781d87cfa0f6b06558e770f981bf7b3afccf2f68
Joseph Briggs <jhbriggs23@gmail.com>
2022-10-05 17:46:58 +0100

    adds structure of methods and classes for the project

---
+++ enhance_greyscale_network.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 17:34:03 2022
+
+@author: josephbriggs
+"""
+
+
+class GreyscaleSuperResModel:
+
+    def __init__(self):
+        pass
+
+    def model(self):
+        '''
+        Implementation of a neural network inspired by
+        https://arxiv.org/pdf/1609.05158.pdf
+
+        Returns
+        -------
+        Model.
+
+        '''
+
+    def model_diagnose(self, high_res_generate, high_res_original):
+        '''
+        return losses and signal to noise ratio.
+        Returns
+        -------
+        None.
+
+        '''
---
+++ train_network.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 17:42:00 2022
+
+@author: josephbriggs
+"""
+
+
+def train_network():
+    '''
+
+
+    Returns
+    -------
+    None.
+
+    '''
+
+
+def test_network():
+    '''
+
+
+    Returns
+    -------
+    None.
+
+    '''
+
+def export_network():
+    '''
+
+
+    Returns
+    -------
+    None.
+
+    '''
