40e16c294f3b4e08a62c99d835ac95a107d3c392
Joseph Briggs <jhbriggs23@gmail.com>
2022-10-06 19:56:43 +0100

    adds a working example of a super resolution neural network

--- enhance_greyscale_network.py
+++ enhance_greyscale_network.py
@@ -6,13 +6,13 @@ Created on Wed Oct  5 17:34:03 2022
 @author: josephbriggs
 """
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
-class GreyscaleSuperResModel:
+class GreyscaleSuperResModel(nn.Module):
 
-    def __init__(self):
-        pass
-
-    def model(self):
+    def __init__(self,res):
         '''
         Implementation of a neural network inspired by
         https://arxiv.org/pdf/1609.05158.pdf
@@ -20,10 +20,34 @@ class GreyscaleSuperResModel:
         Returns
         -------
         Model.
-
         '''
+        super(GreyscaleSuperResModel, self).__init__()
+        self.res = res
+        # three layers
 
-    def model_diagnose(self, high_res_generate, high_res_original):
+        # shape: n_(l-1) * n_l * k_l * k_l
+        # n_l = number of features at layer l
+        # k_l = filter size at layer l
+        # f_l function applied to matrix elementwise
+
+
+        self.conv1 = nn.Conv2d(1, 64, 5,padding='same') # 5x5 kernel, 64 features
+        self.conv2 = nn.Conv2d(64, 32, 3,padding='same') # 3x3 kernel, 32 features
+        self.conv3 = nn.Conv2d(32,res**2,1) #
+
+
+
+    def forward(self,x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        pixel_shuffle = nn.PixelShuffle(self.res)
+        x = pixel_shuffle(x)
+        return x
+
+
+
+    def cost(self, high_res_generate, high_res_original):
         '''
         return losses and signal to noise ratio.
         Returns
--- train_network.py
+++ train_network.py
@@ -5,85 +5,142 @@ Created on Wed Oct  5 17:42:00 2022
 
 @author: josephbriggs
 """
-import math
 from pathlib import Path
 import torch
 from torchvision.io import read_image
-import torchvision.transforms as transforms
-
 from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import enhance_greyscale_network
 
-#%%
-class imageDataset(Dataset):
-    def __init__(self, hr_img_dir, lr_img_dir):
-        self.hr_img_dir = Path(hr_img_dir)
-        self.lr_img_dir = Path(lr_img_dir)
+
+class ImageDataset(Dataset):
+    def __init__(self, high_res_img_dir, low_res_img_dir):
+        self.high_res_img_dir = Path(high_res_img_dir)
+        self.low_res_img_dir = Path(low_res_img_dir)
+        # for i,_ in enumerate(self.low_res_img_dir.glob('*.png')):
+        #     pass
+        # self.length = i
 
     def __len__(self):
-        return len(self.hr_img_dir.glob('*.png'))
+        return 10000  # self.length
 
     def __getitem__(self, idx):
         fname = f"{str(idx):0>7}.png"
-        hr_image = read_image(str(self.hr_img_dir.joinpath(fname)))
-        lr_image = read_image(str(self.lr_img_dir.joinpath(fname)))
-        return hr_image,lr_image
-
-def show_image(image):
-    image_np = image.numpy()    
-    cv2.imshow("image",np.transpose(image_np,axes = [2,1,0]))
-    cv2.waitKey()
-
-def train_network():
-    '''
+        high_res_image = read_image(
+            str(self.high_res_img_dir.joinpath(fname)))/255
+        low_res_image = read_image(
+            str(self.low_res_img_dir.joinpath(fname)))/255
+        return high_res_image, low_res_image
 
 
-    Returns
-    -------
-    None.
+def show_image(low_res_img, high_res_img, learned_img):
 
-    '''
+    _, ax = plt.subplots(1, 3)
+    high_res_img = high_res_img.numpy()
+    low_res_img = low_res_img.numpy()
+    learned_img = learned_img.numpy()
+
+    ax[0].imshow(low_res_img[0, :, :], vmin=0, vmax=1, cmap='gray')
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    ax[0].set_title('LR')
+    ax[1].imshow(high_res_img[0, :, :], vmin=0, vmax=1, cmap='gray')
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
+    ax[1].set_title('HR')
+    ax[2].imshow(learned_img[0, :, :], vmin=0, vmax=1, cmap='gray')
+    ax[2].set_xticks([])
+    ax[2].set_yticks([])
+    ax[2].set_title('Learned')
 
 
-def test_network():
-    '''
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    print(size)
+    for batch, (high_res, low_res) in enumerate(dataloader):
+        # Compute prediction and loss
+        pred = model(low_res)
+        loss = loss_fn(pred, high_res)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(low_res)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-    Returns
-    -------
-    None.
+def test_loop(dataloader, model, loss_fn):
+    num_batches = len(dataloader)
+    test_loss = 0
 
-    '''
+    with torch.no_grad():
+        for high_res, low_res in dataloader:
+            pred = model(low_res)
+            test_loss += loss_fn(pred, high_res).item()
 
-def export_network():
-    '''
-
-
-    Returns
-    -------
-    None.
-
-    '''
+    test_loss /= num_batches
+    print(f"Avg loss: {test_loss:>8f} \n")
 
 
 def main():
-    
-    images = imageDataset('gs_imgs_hr_x2','gs_imgs_lr_x2')
-    
-    imgs_to_show = 3
-    fig,ax = plt.subplots(2,imgs_to_show)
-    ax = ax.ravel()
-    for i in range(imgs_to_show):
-        hr_img,lr_img = images[i]
-        hr_img = hr_img.numpy()
-        lr_img = lr_img.numpy()
-        ax[i].imshow(hr_img[0,:,:], vmin=0, vmax=255,cmap = 'gray')
-        ax[i].set_xticks([])
-        ax[i].set_yticks([])
-        ax[i+imgs_to_show].imshow(lr_img[0,:,:], vmin=0, vmax=255,cmap = 'gray')
-        ax[i+imgs_to_show].set_xticks([])
-        ax[i+imgs_to_show].set_yticks([])
+
+    images = ImageDataset('gs_imgs_hr_x2', 'gs_imgs_lr_x2')
+
+    learning_rate = 1e-2
+    batch_size = 10
+    epochs = 1
+
+    train_count = int(0.7 * len(images))
+    valid_count = int(0.2 * len(images))
+    test_count = len(images) - train_count - valid_count
+
+    train_dataset, valid_dataset, test_dataset =  \
+        torch.utils.data.random_split(
+            images, (train_count, valid_count, test_count))
+
+    train_dataset_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+
+    valid_dataset_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, shuffle=True)
+
+    test_dataset_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=batch_size, shuffle=False)
+
+    model = enhance_greyscale_network.GreyscaleSuperResModel(2)
+    loss = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}\n-------------------------------")
+        train_loop(train_dataset_loader, model, loss, optimizer)
+        test_loop(test_dataset_loader, model, loss)
+    print("Done!")
+
+    test_img = images[0]
+    learned = model(test_img[1]).detach()
+    low_res = test_img[1].detach()
+    high_res = test_img[0].detach()
+
+    show_image(low_res, high_res, learned)
+
+    test_image = cv2.imread(r"crossword.jpeg")[1000:1500, 0:800]
+    test_image_gs = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+
+    test_image_gs_norm = test_image_gs.astype(np.float32)/255
+
+    super_res = model(torch.tensor(test_image_gs_norm).unsqueeze(0))
+
+    cv2.imshow("input", test_image_gs)
+    cv2.imshow("super res", super_res.squeeze(0).detach().numpy())
+
+    cv2.waitKey()
+
+
 if __name__ == "__main__":
     main()
