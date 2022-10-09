9a3f6516c00d4fa783f8d51c41a8e1cabe77461c
Joseph Briggs <jhbriggs23@gmail.com>
2022-10-09 16:27:43 +0100

    creates a tool for generating data sets and a tool for making training sets

---
+++ generate_training_data.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 16:56:55 2022
+
+@author: josephbriggs
+"""
+from pathlib import Path
+import pathlib
+import argparse
+import torch
+import torchvision.transforms as transforms
+from torchvision.io import read_image, write_png
+
+
+def image_data(img_dir, res):
+    stride = 17
+
+    path = Path(img_dir)
+    for image_path in path.glob('*.png'):
+        image = read_image(str(image_path))
+
+        crop_width = image.shape[1] % (stride*res)
+        crop_height = image.shape[2] % (stride*res)
+
+        image = transforms.functional.crop(image, 0, 0,
+                                           image.shape[1]-crop_width,
+                                           image.shape[2]-crop_height)
+
+        hr_image = transforms.Grayscale()(image)
+
+        width = hr_image.shape[1]
+        height = hr_image.shape[2]
+
+        width_lr = int(width/res)
+        height_lr = int(height/res)
+
+        LR_transfrom = torch.nn.Sequential(
+            transforms.Grayscale(),
+            transforms.GaussianBlur(kernel_size=(3, 3), sigma=1),
+            transforms.Resize((width_lr, height_lr)))
+
+        lr_image = LR_transfrom(image)
+
+        for i in range(0, width_lr, stride):
+            for j in range(0, height_lr, stride):
+                crop_hr = hr_image[:, i*res:i*res+stride*res,
+                                   j*res:j*res+stride*res]
+                crop_lr = lr_image[:, i:i+stride, j:j+stride]
+
+                yield crop_hr, crop_lr
+
+
+def main():
+    '''
+    Converts files to greyscale.
+    '''
+
+    parser = argparse.ArgumentParser(description='Convert files to greyscale.')
+    parser.add_argument('--input_path', "-i", type=str,
+                        help='path to the image or directory of images. \
+                If converting a directory, use *')
+    parser.add_argument('--output_path', "-o", type=str,
+                        help='prefix of path to the where the greyscale images will be saved.')
+    parser.add_argument('--res', "-r", type=int,
+                        help='downscale factor.')
+
+    args = parser.parse_args()
+
+    pathlib.Path(args.output_path +
+                 f'_hr_x{args.res}').mkdir(parents=True, exist_ok=True)
+    pathlib.Path(args.output_path +
+                 f'_lr_x{args.res}').mkdir(parents=True, exist_ok=True)
+
+    images = image_data(args.input_path, res=args.res)
+
+    for i, (hr, lr) in enumerate(images):
+        write_png(hr, args.output_path+f'_hr_x{args.res}/{str(i):0>7}.png')
+        write_png(lr, args.output_path+f'_lr_x{args.res}/{str(i):0>7}.png')
+
+    print('converted files to greyscale')
+
+
+if __name__ == "__main__":
+
+    main()
--- imgs_to_gs.py
+++ imgs_to_gs.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -5,51 +6,9 @@ Created on Wed Oct  5 16:56:55 2022
 
 @author: josephbriggs
 """
-from pathlib import Path
 import pathlib
 import argparse
-import torch
-import torchvision.transforms as transforms
-from torchvision.io import read_image,write_png
-
-def image_data(img_dir,res):
-    stride = 17
-    
-    path = Path(img_dir)
-    for image_path in path.glob('*.png'):
-        image = read_image(str(image_path))
-        
-        crop_width = image.shape[1] % (stride*res)
-        crop_height = image.shape[2] % (stride*res)
-        
-        image = transforms.functional.crop(image,0,0,
-                                           image.shape[1]-crop_width,
-                                           image.shape[2]-crop_height)
-        
-        hr_image = transforms.Grayscale()(image)
-        
-        width = hr_image.shape[1]
-        height = hr_image.shape[2]
-        
-        width_lr = int(width/res)
-        height_lr = int(height/res)
-        
-        LR_transfrom = torch.nn.Sequential(
-        transforms.Grayscale(),
-        transforms.GaussianBlur(kernel_size=(3, 3), sigma=1),
-        transforms.Resize((width_lr,height_lr)))
-        
-        
-        lr_image = LR_transfrom(image)
-        
-        for i in range(0,width_lr,stride):
-            for j in range(0,height_lr,stride):
-                crop_hr = hr_image[:,i*res:i*res+stride*res,
-                                   j*res:j*res+stride*res]            
-                crop_lr = lr_image[:,i:i+stride,j:j+stride]       
-
-                yield crop_hr,crop_lr
-
+import cv2
 
 
 def main():
@@ -62,25 +21,29 @@ def main():
                         help='path to the image or directory of images. \
                 If converting a directory, use *')
     parser.add_argument('--output_path', "-o", type=str,
-                        help='prefix of path to the where the greyscale images will be saved.')
+                        help='output path where images will be saved.')
     parser.add_argument('--res', "-r", type=int,
-                    help='downscale factor.')
+                        help='downscale factor.')
 
     args = parser.parse_args()
 
-    pathlib.Path(args.output_path+f'_hr_x{args.res}').mkdir(parents=True, exist_ok=True)
-    pathlib.Path(args.output_path+f'_lr_x{args.res}').mkdir(parents=True, exist_ok=True)
+    pathlib.Path(args.output_path).mkdir(parents=True, exist_ok=True)
 
-    images = image_data(args.input_path,res = args.res)
+    # files = pathlib.Path(args.input_path).glob(r'/*.png|')
+    file_extentions = ['png', 'jpeg', 'jpg']
+    files = []
+    for file_extension in file_extentions:
+        files += pathlib.Path(args.input_path).glob(fr'*.{file_extension}')
 
-    for i,(hr,lr) in enumerate(images):
-        write_png(hr, args.output_path+f'_hr_x{args.res}/{str(i):0>7}.png')
-        write_png(lr, args.output_path+f'_lr_x{args.res}/{str(i):0>7}.png')
+    for file in files:
+        file_name = file.name
+        image = cv2.imread(str(file))
+        image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        file_name_to_save = args.output_path+"/"+file_name
+        print(file_name_to_save)
+        cv2.imwrite(file_name_to_save, image_gs)
 
     print('converted files to greyscale')
-
-
-
 
 
 if __name__ == "__main__":
